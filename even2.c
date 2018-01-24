2b3eb19f3718632a8411cf678839c647d76d548a
umashanmugam <35134814+umashanmugam@users.noreply.github.com>
2018-01-24 16:35:18 +0530

    Create even2.c

---
+++ even2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int f,l.i;
+	printf("Enter the 1st&last values");
+	scanf("%d%d",&f,&l);
+	for(i=f;i<=l;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d",i);
+		}
+	}
+return 0;
+}
