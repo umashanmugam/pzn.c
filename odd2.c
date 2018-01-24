e50ecfd5c5b05995a9e7448224981eaee9f9f77d
umashanmugam <35134814+umashanmugam@users.noreply.github.com>
2018-01-24 16:31:25 +0530

    Create odd2.c

---
+++ odd2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int f,l.i;
+	printf("Enter the 1st&last values");
+	scanf("%d%d",&f,&l);
+	for(i=f+1;i<=l;i++)
+	{
+		if(i%2==1)
+		{
+			printf("%d",i);
+		}
+	}
+return 0;
+}
