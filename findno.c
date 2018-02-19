60c8137ab21834cb30ea087eebd55893517a1bdf
umashanmugam <35134814+umashanmugam@users.noreply.github.com>
2018-02-19 16:27:52 +0530

    Create findno.c

---
+++ findno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	{
+	           if((N>=1)&&(N<=10))		{
+			printf("Yes %d is present",N);
+		}
+		else
+		{
+			printf("No %d is not present",N);
+		}
+	}
+	return 0;
+	
+}
